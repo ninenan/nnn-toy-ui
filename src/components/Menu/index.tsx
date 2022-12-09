51d0d87688727fb5f0d0757c4a4c896858a53994
久南 <wuqingnan.wqn@digital-engine.com>
2022-12-09 14:52:03 +0800

    feat: Menu

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,14 +1,14 @@
-import { FC } from 'react';
+import { FC, PropsWithChildren } from 'react';
 import Menu, { type IMenuProps } from './menu';
 import SubMenu, { type ISubMenuProps } from './subMenu';
 import MenuItem, { type IMenuItemProps } from './menuItem';
 
-export type IMenuCom = FC<IMenuProps> & {
-  Item: FC<IMenuItemProps>;
-  SubMenu: FC<ISubMenuProps>;
+export type IMenuCom = FC<PropsWithChildren<IMenuProps>> & {
+  Item: FC<PropsWithChildren<IMenuItemProps>>;
+  SubMenu: FC<PropsWithChildren<ISubMenuProps>>;
 };
 
-const MenuCom = Menu as IMenuCom as any;
+const MenuCom = Menu as IMenuCom;
 
 MenuCom.Item = MenuItem;
 MenuCom.SubMenu = SubMenu;
